398bd2dbf0dc4558289019b27d5a4267745b3e76
ext.alyubetskiy <ext.alyubetskiy@stash.tcsbank.ru>
2018-04-10 18:00:56 +0300

    Initial commit

---
+++ src/Component.java
@@ -0,0 +1,57 @@
+public class Component {
+
+    private double xPosition;
+    private double yPosition;
+    private int width;
+    private int height;
+    private String text;
+
+    public Component(double xPosition, double yPosition, int width, int height, String text) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.width = width;
+        this.height = height;
+        this.text = text;
+    }
+
+    public double getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition() {
+        this.xPosition = xPosition;
+    }
+
+    public double getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition() {
+        this.yPosition = yPosition;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth() {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight() {
+        this.height = height;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText() {
+        this.text = text;
+    }
+
+}
